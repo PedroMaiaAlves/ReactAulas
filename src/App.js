e134fc900f2cc3e6ddba22f4f3e09abaacb2c0cd
Pedro Henrique Maia Alves <parodianos97@gmail.com>
2025-06-04 20:17:56 -0300

    Aula 12 rederizacao por condicao

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-
+import Condicional from './components/Condicional';
 import './App.css';
-import Eventos from './components/Eventos';
-import Form from './components/Form';
 
 function App() {
 
   return (
     <div className="App">
-      <h1>Testando eventos</h1>
-      <Eventos />
-      <Form />
+      <h1>Renderização Condicional</h1>
+      <Condicional/>
     </div>
   );
 }
