22c388c984a9614875e013764e8d694722567d3c
Pedro Henrique Maia Alves <parodianos97@gmail.com>
2025-06-05 08:27:08 -0300

    Aula 15 React - Router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Contato from './pages/Contato';
+import Empresa from './pages/Empresa';
 import './App.css';
-import SeuNome from './components/SeuNome';
-import Saudacao from './components/Saudacao';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 function App() {
-
-  const [nome, setNome] = useState()
-
   return (
-    <div className="App">
-      <h1>State Lift</h1>
-      <SeuNome setNome = {setNome} />
-      <Saudacao nome = {nome}/>
-    </div>
-  );
+  <Router>
+    <Navbar/>
+    <Routes>
+      <Route exact path='/' element={<Home/>}/>
+      <Route path='/empresa' element={<Empresa/>}/>
+      <Route path='/contato' element={<Contato/>}/>
+    </Routes>
+    <Footer/>
+  </Router>
+  )
 }
 
 export default App;
---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,5 @@
+function Footer(){
+    return<footer>Rodapé da aplicação</footer>
+}
+
+export default Footer
