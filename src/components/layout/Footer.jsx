064db13519c8e0e8f47c98acd485e42856e40cec
Pedro Henrique Maia Alves <parodianos97@gmail.com>
2025-06-09 10:57:37 -0300

    React Icons Aula 16

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,5 +1,16 @@
+import {FaFacebook, FaInstagram, FaWhatsapp} from 'react-icons/fa'
+
+import styles from './Footer.module.css'
+
 function Footer(){
-    return<footer>Rodapé da aplicação</footer>
+    return<footer>Rodapé da aplicação
+        <ul className={styles.social_list}>
+            <li><FaFacebook/></li>
+            <li><FaInstagram/></li>
+            <li><FaWhatsapp/></li>
+        </ul>
+            <p>Nosso rodapé</p>
+    </footer>
 }
 
 export default Footer
